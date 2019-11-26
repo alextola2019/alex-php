c07451806da73e88f2e676d0afb5962ac041cebd
AS3-2-20 <AS3-2-20@uni.lan>
2019-11-26 08:14:37 +0100

    `P

--- AlexPHP/alextolaejer1.sql
+++ AlexPHP/alextolaejer1.sql
@@ -7,7 +7,7 @@ CREATE TABLE login (
   password varchar(200) 
     );
 create table texto (
-  id int(200) auto_increment,
+  id int auto_increment,
   titulo varchar(200),
   imagen varchar(200),
   texto varchar(200)
